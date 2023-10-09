6616c2878822317658c32d793a576596c1dacfeb
rinormaloku <rinormaloku37@gmail.com>
2023-10-09 23:26:19 +0200

    improvRefactored ed readability

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 ## Performance of OPA
 
+### Setup the data
+
+Generate `data.json` that OPA uses to evaluate API Keys.
+
+```bash
+go run generate/keys.go
+```
+
+This stores the file in `./bundle/apikeys/data.json`
+
+Proceed to recreate the optimized OPA bundle.
+
+```bash
+bundle/bundle.sh
+```
+
+Next, we will validate OPA in two ways:
+1. Consume the Bundle locally
+2. Poll a WebServer that hosts the Bundle
+
+### Consume the Bundle locally
+
 Run the program:
 ```bash
-go run main.go
+go run localdata/local.go
 ```
 
 Output:
@@ -13,26 +35,20 @@ API Keys loaded into memory.
 
 Allowed count:  50
 Rejected count:  50
-2023/03/06 14:27:17 Avg evaluation took: 10.577µs
+2023/03/06 14:27:17 Avg evaluation took: 12ms
 ```
 
-## Reproduce the test
+### Poll a WebServer that hosts the Bundle
 
-Generate `data.json` that OPA uses to evaluate API Keys.
+In a second terminal switch to the `bundle` directory and run the following command:
 
 ```bash
-go run generate/keys.go
+cd bundle
+python3 -m http.server 8981
 ```
 
-This stores the data.json in `./bundle/apikeys/data.json`
-
-Proceed to recreate the optimized OPA bundle.
+In the first terminal, from the root of the repo run the following command:
 
 ```bash
-bundle/bundle.sh
+go run polldata/poll.go
 ```
-
-And then you can run the program as shown at the top of the article.
-
-Alternatively, you can run the http server to host the bundle.
-
--- localdata/local.go
+++ localdata/local.go
@@ -5,11 +5,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/open-policy-agent/opa/sdk"
+	"github.com/open-policy-agent/opa/rego"
 	"github.com/satori/go.uuid"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -17,56 +18,45 @@ type Data struct {
 	ApiKeys map[string]interface{} `json:"ApiKeys"`
 }
 
-const (
-	apiKeysCount     = 1000000
-	totalKeysToCheck = 100
-	halfKeysToCheck  = totalKeysToCheck / 2
-)
+const totalKeysToCheck = 100
+const halfKeysToCheck = totalKeysToCheck / 2
 
 func main() {
-	ctx := context.Background()
 	fmt.Println("Loading API Keys into memory...")
 
-	opa, err := sdk.New(ctx, sdk.Options{
-		Config: bytes.NewReader([]byte(`
-services:
-  - name: hostedbundle2
-    url: http://0.0.0.0:8981/
-
-bundles:
-  authz:
-    service: hostedbundle2
-    resource: bundle.tar.gz
-    polling:
-      min_delay_seconds: 60
-      max_delay_seconds: 120
-`))})
-	if err != nil {
-		log.Fatalf("Failed initializing SDK: %v", err)
-	}
-
+	preparedQuery := prepareRegoQuery()
 	existingKeys := getExistingKeys()
+
 	differentInputs := appendInvalidKeysAndShuffle(existingKeys)
 
 	start := time.Now()
-	fmt.Println("API Keys loaded into memory.")
-	// Test the 100 api keys, 50 should be accepted, and 50 should be rejected
-	allowedCount, rejectedCount := evaluateApiKeys(opa, ctx, differentInputs)
+	allowedCount, rejectedCount := evaluateApiKeys(preparedQuery, differentInputs)
 	elapsedTime := time.Since(start).Microseconds()
+
 	fmt.Printf("\nAllowed count: %d\n", allowedCount)
 	fmt.Printf("Rejected count: %d\n", rejectedCount)
-	fmt.Printf("Avg evaluation took: %d microseconds\n", elapsedTime/totalKeysToCheck)
+	fmt.Printf("Avg evaluation took: %dms\n", elapsedTime/totalKeysToCheck)
+
+	runtime.GC()
+	printMemUsage()
+}
+
+func prepareRegoQuery() *rego.PreparedEvalQuery {
+	base := rego.New(rego.Query("data.apikeys.allow"), rego.LoadBundle("./bundle/bundle.tar.gz"))
+	preparedQuery, err := base.PrepareForEval(context.Background())
+	if err != nil {
+		log.Fatalf("Failed preparing rego query: %v", err)
+	}
+	return &preparedQuery
 }
 
 func getExistingKeys() []string {
-	// Read data in order to have valid inputs
-	// So that we include in performance evaluation successfull cases and failures
-	file, err := ioutil.ReadFile("bundle/apikeys/data.json")
+	file, err := ioutil.ReadFile("./bundle/apikeys/data.json")
 	if err != nil {
 		log.Fatalf("Failed reading file: %v", err)
 	}
 
-	var data Data
+	data := Data{}
 	if err := json.Unmarshal(file, &data); err != nil {
 		log.Fatalf("Failed unmarshalling data: %v", err)
 	}
@@ -84,7 +74,7 @@ func appendInvalidKeysAndShuffle(allApiKeys []string) []interface{} {
 	var differentInputs []interface{}
 	for i := 0; i < halfKeysToCheck; i++ {
 		// Add existing api keys picked randomly from data.json
-		randKey := allApiKeys[rand.Intn(apiKeysCount/2)]
+		randKey := allApiKeys[rand.Intn(halfKeysToCheck)]
 		differentInputs = append(differentInputs, createInput(randKey))
 
 		// Add non-existing api keys
@@ -103,20 +93,34 @@ func createInput(key string) interface{} {
 	return input
 }
 
-func evaluateApiKeys(opa *sdk.OPA, ctx context.Context, differentInputs []interface{}) (int, int) {
+func evaluateApiKeys(preparedQuery *rego.PreparedEvalQuery, differentInputs []interface{}) (int, int) {
 	allowedCount := 0
 	rejectedCount := 0
 	for _, input := range differentInputs {
-		decision, err := opa.Decision(ctx, sdk.DecisionOptions{Path: "/apikeys/allow", Input: input})
+		result, err := preparedQuery.Eval(context.Background(), rego.EvalInput(input))
 		if err != nil {
 			log.Println("Error in decision:", err)
 			continue
 		}
-		if result, ok := decision.Result.(bool); ok && result {
+		if result.Allowed() {
 			allowedCount++
 		} else {
 			rejectedCount++
 		}
 	}
 	return allowedCount, rejectedCount
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
